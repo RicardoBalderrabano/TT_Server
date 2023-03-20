54d0f4c42b9f06b4efe5edd122d0598a5584cbdf
Ricardo Balderrabano <rickyxrl8@gmail.com>
2023-03-20 19:30:08 +0000

    commit appi

---
+++ config_app.py
@@ -0,0 +1,10 @@
+class DevelopmentConfig():
+    DEBUG=False
+    MYSQL_HOST='140.84.179.17'
+    MYSQL_USER='admin'
+    MYSQL_PASSWORD='crCY$!55C6_a35D?'
+    MYSQL_DB='ROSTROS'
+    
+config={
+    'development':DevelopmentConfig
+}
