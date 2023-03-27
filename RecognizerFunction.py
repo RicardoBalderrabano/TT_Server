afa214a4997f539dacce574b662c80209fd4a376
Ricardo Balderrabano <rickyxrl8@gmail.com>
2023-03-27 04:09:45 +0000

    26/03 userfound working

--- RecognizerFunction.py
+++ RecognizerFunction.py
@@ -10,51 +10,10 @@ from flask import request
 from config_app import config
 from flask_mysqldb import MySQL
 import json
-from RecognizerFunction import addencodingsJ
 
 
-app = Flask(__name__)
-#encodingsJ=[{'encoding':'12 415 45 45'}]
-conexion=MySQL(app)
 
-@app.route('/users')
-def list_users():
-   try:
-      cursor=conexion.connection.cursor()
-      sql="SELECT PersonID, LastName, FirstName FROM Users"
-      cursor.execute(sql)
-      datos=cursor.fetchall()
-      persons=[]
-      for fila in datos:
-         person={'ID':fila[0], 'LastName':fila[1], 'FirstName':fila[2]}
-         persons.append(person)
-      return jsonify({'persons':persons, 'message': 'Usuarios listados'})
-
-   except Exception as ex:
-      print(ex)
-      return jsonify({'message':'x'})
-
-@app.route('/users/<PersonID>', methods=['GET'])
-def read_person(PersonID):
-   try:
-      cursor=conexion.connection.cursor()
-      sql="SELECT PersonID, LastName, FirstName FROM Users WHERE PersonID = '{0}'".format(PersonID)
-      cursor.execute(sql)
-      datos=cursor.fetchone()
-      if datos != None:
-         person={'ID':datos[0], 'LastName':datos[1], 'FirstName':datos[2]}
-         return jsonify({'person':person, 'message': 'Usuario encontrado'})
-      else:
-         return jsonify({'message': 'Usuario no encontrado'})
-   except Exception as ex:
-      return jsonify({'message':'ERROR 2'})
-
-       
-
-@app.route('/encodings', methods=['POST'])   # Getting POST request from Raspberry Pi (Encoding)
-# FUNCTION FOR FACE RECOGNITION
-
-def addencodingsJ():
+def addencodingsJ(encs):
 
    FACEDB = "/home/ubuntu/tests/facedatabase2.dat"  # Direction of the database of trained faces
    
@@ -118,23 +77,5 @@ def addencodingsJ():
    # loop over the recognized faces
    print(id)
    
-   # Database connection
-   PersonID=id
-   cursor=conexion.connection.cursor()
-   sql="SELECT PersonID, LastName, FirstName FROM Users WHERE PersonID = '{0}'".format(PersonID)
-   cursor.execute(sql)
-   datos=cursor.fetchone()
-   if datos != None:
-      person={'LastName':datos[1], 'FirstName':datos[2]}
-      return jsonify({'person':person, 'message':'UserFound'})
-   else:
-       return jsonify({'message':'UserNoFound'})
-
-def page_no_found(error):
-   return "<h1>La pagina que intentas buscar no existe...</h1>", 404
-  
-
-if __name__ == "__main__":
-   app.config.from_object(config['development'])
-   app.register_error_handler(404,page_no_found)
-   app.run(host='0.0.0.0',port=80)
+   #encodingsJ.append(encodingJ)
+   return jsonify({'id':id}) 
