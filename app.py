9a172bd19e7009f34157cbe7623be920a18e7dce
Ricardo Balderrabano <rickyxrl8@gmail.com>
2023-03-30 16:40:55 +0000

    updaate tables working

--- app.py
+++ app.py
@@ -101,25 +101,93 @@ def LockerStatus():
    lockerfreeJs={'LockerID': lockerfree[0], 'DirectionF':lockerfree[1]} # LockerID, Direction to be opened
    return jsonify({'Lockers': lockall, 'LockerFree':lockerfreeJs}) # Lockers registered and Locker ID/Direction to be opened
   
-# Getting POST for update LockersUsers Table in the DB
+# Getting POST to check user locker
+@app.route('/CheckLocker', methods=['POST']) # CHECKING LOCKER-USER 
+# FUNCTION TO RETRIEVE INFROMATION OF LockersUsers TABLE
+def checklocker():
+   
+      UserID=request.json['UserID']  # Getting UserID from the json.request file
+      print(UserID)
+      cursor=conexion.connection.cursor()
+      sql="SELECT LockerID, LockerUsed, PersonID, DateAndTime FROM LockersUsers AS Ordered_date WHERE DateAndTime=ANY(SELECT MAX(DateAndTime) FROM LockersUsers GROUP BY PersonID) AND PersonID='{0}'".format(UserID)
+      cursor.execute(sql)
+      LastLockerStatus=cursor.fetchone()
+      #LastLocker=LastLockerStatus[1]
+      if LastLockerStatus is None:
+         #ASIGN NEW LOCKER
+         openflag=0 # Just open locker option
+         sql2="SELECT LockerID, LockerNum FROM Lockers WHERE LockerNum=(SELECT MIN(LockerNum) FROM Lockers WHERE Availability=1)" 
+         cursor.execute(sql2)
+         lockerfree=cursor.fetchone()
+         lockerfreeJs={'LockerID': lockerfree[0], 'DirectionF':lockerfree[1]} # LockerID, Direction to be opened
+         return jsonify({'LockerFree':lockerfreeJs, 'Openflag': openflag}) # Lockers registered and Locker ID/Direction to be opened
+      
+      else:
+         LastLocker=LastLockerStatus[1]
+         if LastLocker==1:
+            #ASIGN THE SAME LOCKER
+            openflag=1 #Open locker and leave option
+            lockerfree=LastLockerStatus[0]
+            lockerfreeJs={'LockerID':lockerfree}
+            return jsonify({'LockerFree': lockerfreeJs, 'Openflag': openflag})
+         elif LastLocker==2:
+            #ASIGN NEW LOCKER
+            openflag=0 # Just open locker option
+            sql2="SELECT LockerID, LockerNum FROM Lockers WHERE LockerNum=(SELECT MIN(LockerNum) FROM Lockers WHERE Availability=1)" 
+            cursor.execute(sql2)
+            lockerfree=cursor.fetchone()
+            lockerfree=LastLockerStatus[0]
+            lockerfreeJs={'LockerID': lockerfree, 'DirectionF':lockerfree[1]} # LockerID, Direction to be opened
+            return jsonify({'LockerFree':lockerfreeJs, 'Openflag': openflag}) # Lockers registered and Locker ID/Direction to be opened
+         else:
+            return jsonify({'LockerFree': 'ERROR', 'Openflag': 'ERROR'})
+
+# Getting POST to update LockersUsers Table in the DB
 @app.route('/updateRegister', methods=['POST']) # UPDATING DB BITACORA
 # FUNCTION FOR INSERT DATA WHEN A LOCKER IS OPENED (UserID, LockerID, DATE and TIME)
 def lockerUsability():
-   try:
-      now=datetime.now()   #Getting date and time from the server
-      formatted_date=now.strftime('%Y-%m-%d %H:%M:%S') #Setting format of the data and time 
+   #try:
+      now=datetime.now()   # Getting date and time from the server
+      formatted_date=now.strftime('%Y-%m-%d %H:%M:%S') # Setting format of the data and time 
       cursor=conexion.connection.cursor()
-      UserID=request.json['UserID'] #Getting UserID from the json.request file
+      UserID=int(request.json['UserID']) # Getting UserID from the json.request file
       LockerID2 = request.json['LockerID']  # Getting LockerID from the json.request file
-      # INSERT DATA INTO THE TABLE (Instruction)   
-      sql3="INSERT LockersUsers (LockerID, PersonID, DateAndTime) VALUES (%s,%s,%s)"
-      input_date=(LockerID2, UserID, formatted_date)
-      cursor.execute(sql3,input_date)
-      conexion.connection.commit() #Updating Finished 
+      Leaveflag=int(request.json['Leaveflag'])
+      print(Leaveflag)
+      if Leaveflag==1:
+         # INSERT DATA INTO THE TABLE (Instruction)   
+         sql3="INSERT LockersUsers (LockerID, PersonID, DateAndTime, LockerUsed) VALUES (%s,%s,%s,%s)"
+         input_date=(LockerID2, UserID, formatted_date, Leaveflag)
+         cursor.execute(sql3,input_date)
+         conexion.connection.commit() #Updating Finished
+         # UPDATE LOCKER STATUS
+         sql4="UPDATE Lockers SET Availability=2 WHERE LockerID='{0}'".format(LockerID2)
+         cursor.execute(sql4)
+         conexion.connection.commit() #Updating Finishe
+         return jsonify({'message':'Registro Done'})
+      else:
+         print('Aqui')
+         print(LockerID2)
+         print(UserID)
+         print(formatted_date)
+         print(Leaveflag)
+         
+         sql5="INSERT LockersUsers (LockerID, PersonID, DateAndTime, LockerUsed) VALUES (%s,%s,%s,%s)"
+         input_date2=(LockerID2, UserID, formatted_date, Leaveflag)
+         cursor.execute(sql5,input_date2)
+         conexion.connection.commit() #Updating Finished'''
+         
+         # UPDATE LOCKER STATUS
+         sql6="UPDATE Lockers SET Availability=1 WHERE LockerID='{0}'".format(LockerID2)
+         cursor.execute(sql6)
+         conexion.connection.commit() #Updating Finished
+         return jsonify({'message':'Registro Done'})
+
+      
+      #conexion.connection.commit() #Updating Finished 
       print("User registered successfully")
       return jsonify({'message':'Registro Done'}) 
-   except Exception as ex:
-      return jsonify({'message':'ERROR registro'})
+   
 
 
 @app.route('/encodings/Lockers', methods=['POST'])   # Getting POST request from Raspberry Pi Lockers (Encoding)
